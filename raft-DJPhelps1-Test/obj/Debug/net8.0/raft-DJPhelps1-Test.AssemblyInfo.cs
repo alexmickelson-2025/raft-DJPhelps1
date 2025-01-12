6e934eeb0ec6db57b7651d92467e906990ed5ad4
DJPhelps1 <46822189+DJPhelps1@users.noreply.github.com>
2025-01-11 22:58:31 -0700

    Wahoo, git add .gitignore !

--- raft-DJPhelps1-Test/obj/Debug/net8.0/raft-DJPhelps1-Test.AssemblyInfo.cs
+++ raft-DJPhelps1-Test/obj/Debug/net8.0/raft-DJPhelps1-Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("raft-DJPhelps1-Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+102975b8cf1e3c4d4dee0c382eb9800c78023651")]
 [assembly: System.Reflection.AssemblyProductAttribute("raft-DJPhelps1-Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("raft-DJPhelps1-Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
